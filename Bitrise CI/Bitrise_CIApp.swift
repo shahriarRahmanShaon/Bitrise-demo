6d901398b901c632bb2a97a520026aaa9bf7d43e
shahriarRahmanShaon <shaon463@gmail.com>
2022-03-06 09:33:14 +0600

    Initial Commit

---
+++ Bitrise CI/Bitrise_CIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitrise_CIApp.swift
+//  Bitrise CI
+//
+//  Created by sergio shaon on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Bitrise_CIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
